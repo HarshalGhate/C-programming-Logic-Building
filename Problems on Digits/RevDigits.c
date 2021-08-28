4d093e200041f43d5ffc4bf38a6b3374c1f17959
HarshalGhate <87290454+HarshalGhate@users.noreply.github.com>
2021-08-28 17:15:28 +0530

    RevDigits

--- Problems on Digits/RevDigits.c
+++ Problems on Digits/RevDigits.c
@@ -1,8 +1,14 @@
 /*
-Write a program which accept number from user and display all Digits.
-Input: 7521
-output: 1       2       5       7
+1.Write a program which accept number from user and display its digits in reverse
+order.
+Input : 2395
+Output :
+5
+9
+3
+2
 */
+
 #include<stdio.h>
 void DisplayDigits(int iNo)
 {
