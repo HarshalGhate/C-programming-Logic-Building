c0efef8edfff74c2f305d34490d263729e4d2108
HarshalGhate <87290454+HarshalGhate@users.noreply.github.com>
2021-09-24 16:33:01 +0530

    Add files via upload

---
+++ Pattern_Printing_basics/Pattern6.c
@@ -0,0 +1,29 @@
+//Aceept number from user and display below pattern.
+//Input: 5
+//Output: A B C D E
+
+#include<stdio.h>
+void Pattern(int iNo)
+{	int iCnt=0;
+	char iCh='A';
+	if(iNo<0)
+	{
+		iNo=-iNo;
+	}
+	for(iCnt=1;iCnt<=iNo;iCnt++)
+	{
+		printf("%c\t",iCh++);
+	}
+
+}
+
+int main()
+{
+	int iValue=0;
+	printf("Enter the number of elements:");
+	scanf("%d",&iValue);
+	Pattern(iValue);
+	return 0;
+}
+
+
