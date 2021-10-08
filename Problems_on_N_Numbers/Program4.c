b523702e44789281e0f0969f84bde75b6b15c371
HarshalGhate <87290454+HarshalGhate@users.noreply.github.com>
2021-10-08 11:40:55 +0530

    Add files via upload

---
+++ Problems_on_N_Numbers/Program4.c
@@ -0,0 +1,52 @@
+/*
+4. Accept N numbers from user and display all such elements which are 
+divisible by 3 and 5. 
+Input : N : 6 
+ Elements : 85 66 3 15 93 88 
+Output : 15 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int *Arr,int iSize)
+{   
+    int i=0;
+   printf("Divisible by 3 and 5 elements are:\n");
+    for(i=0;i<iSize;i++)
+    {
+        if((Arr[i]%5==0)&&(Arr[i]%3==0))
+        {
+            printf("%d\t",Arr[i]);
+        }
+    }
+
+}
+int main()
+{
+    int *P=NULL;
+    int iLength=0,i=0;
+
+    printf("Enter the number of elements :\n");
+    scanf("%d",&iLength);
+
+    P=(int*)malloc(iLength*sizeof(int));
+
+    if(P==NULL)
+    {
+        printf("Unable to allcocate Memory\n");
+        return -1;
+    }
+
+    printf("Enter the Elements :\n");
+    for(i=0;i<iLength;i++)
+    {
+        scanf("%d",&P[i]);
+    }
+    
+    Display(P,iLength);
+
+    free(P);
+
+    return 0;
+}
