39f681698c4673d9b7c2ccb70e49958d1ab77463
HarshalGhate <87290454+HarshalGhate@users.noreply.github.com>
2021-11-05 12:34:47 +0530

    Add files via upload

---
+++ Problems_On_string_or_char/Question22.c
@@ -0,0 +1,41 @@
+/*
+2.Write a program which accept string from user and accept one 
+character. Return frequency of that character. 
+Input : “Marvellous Multi OS” 
+ M 
+Output : 2 
+Input : “Marvellous Multi OS” 
+ W 
+Output : 0
+*/
+
+#include<stdio.h>
+
+int CountChar(char *str,char ch)
+{
+   int iCnt=0;
+   while(*str!='\0')
+   {
+       if(*str==ch)
+       {
+           iCnt++;
+       }
+       str++;
+   }
+   return iCnt;
+}
+
+int main()
+{
+    char arr[30];
+    char cValue='\0';
+    int iRet=0;
+    printf("Enter the string :\n");
+    scanf("%[^'\n']s",arr);
+    printf("Enter the character to check :\n");
+    scanf(" %c",&cValue);
+    iRet=CountChar(arr,cValue);
+    printf("%d",iRet);
+   
+    return 0;
+}
